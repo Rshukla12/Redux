4e69713ada729a620a723c842c4d14c305d8a5c1
Ravi Shukla <rshuklabbb@gmail.com>
2021-12-28 16:16:37 +0530

    Swapped the custom thunk middle ware with redux thunk

--- src/Redux Thunk/Redux/store.js
+++ src/Redux Thunk/Redux/store.js
@@ -1,19 +1,20 @@
 import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import { appReducer } from "./App/reducer";
 import { authReducer } from "./Authentication/reducer";
+import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({ auth: authReducer, app: appReducer } )
 
-const thunkMiddleware = (store) => (next) => (action) => {
-    return typeof action === "function"
-      ? action(store.dispatch, store.getState)
-      : next(action);
-};
+// const thunkMiddleware = (store) => (next) => (action) => {
+//     return typeof action === "function"
+//       ? action(store.dispatch, store.getState)
+//       : next(action);
+// };
 
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 export const store = createStore( rootReducer, composeEnhancers(
     applyMiddleware(
-        thunkMiddleware
+        thunk
     )
 ) );
